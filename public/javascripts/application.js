11bb4e9b4725491fbc939221aab8429d9202a081
petrero <petrero@gmail.com>
2012-07-21 17:29:40 +0300

    Toggle task checkboxes through four states with the help of a plugin

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,39 @@
+jQuery.fn.extend({
+	
+	taskStates: ['task-empty', 'task-x', 'task-apostrophe', 'task-dash'],
+
+	resetTaskStateClassNames: function(){
+		var elements = this;
+		jQuery.each(jQuery.fn.taskStates, function(){
+			elements.removeClass(this);
+		});
+		return this;
+	},
+
+	resetTaskState: function(){},
+
+	toggleTaskState: function(){
+		this.resetTaskStateClassNames();
+
+		return this.each(function(){
+			var element = jQuery(this);
+			var taskStateIndex = element.data('taskStateIndex') || 0;
+			taskStateIndex = (taskStateIndex + 1) % jQuery.fn.taskStates.length;
+
+			element.data('taskStateIndex', taskStateIndex)
+				.addClass(jQuery.fn.taskStates[taskStateIndex])
+		});
+	}
+});
+
 jQuery(function () {
+
+	$('.completion a').live("click", function(){
+		$(this).toggleTaskState();
+		return false;
+	});
+
+
 	$('#add').click(function(e){
 		var taskItem = $('#tasks ul li:first').clone();
 		taskItem.find('input[type="text"]').val("");
@@ -9,7 +44,7 @@ jQuery(function () {
 
 	$('#add').click().click().text();
 
-	$('#tasks ul').sortable({handle:".handle"});
+	$('#tasks ul').sortable({handle:".handle"}).disableSelection();
 	
 	$('input[type="text"]:first').focus();
 });
